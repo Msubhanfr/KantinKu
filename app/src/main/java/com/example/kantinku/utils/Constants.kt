4c06fdb2e0eb71ed4565a68d66440d51c4efd9a9
Moch Soebhan Faturrachman <mochsubhanfaturrachman@gmail.com>
2026-05-11 21:58:40 +0700

    Delete app directory

--- app/src/main/java/com/example/kantinku/utils/Constants.kt
+++
@@ -1,9 +0,0 @@
-package com.example.kantinku.utils
-
-object Constants {
-    const val EXTRA_MENU_ID = "extra_menu_id"
-    const val EXTRA_MENU_NAME = "extra_menu_name"
-    const val EXTRA_MENU_PRICE = "extra_menu_price"
-
-    val CATEGORIES = listOf("Semua", "Makanan", "Minuman", "Snack")
-}
